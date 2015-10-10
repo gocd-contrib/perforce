d81cc49be5d31c62478f62e395cc11dcabf2e0b1
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-10-11 01:11:57 +0530

    Fix paths, again

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default['perforce']['version'] = '09.2'
 
 case node[:os]
 when "linux"
-  default['perforce']['bin_dir']  = '/usr/local'
+  default['perforce']['bin_dir']  = '/usr/local/bin'
 when "windows"
   default['perforce']['bin_dir'] = 'C:\Program Files (x86)\Perforce'
 end
