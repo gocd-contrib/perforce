0b2d0c34413427ca833414e574cd23893bd0d6e0
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-10-11 00:33:13 +0530

    Create the perforce bin_dif if it does not exist

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,10 @@
 p4_exe = node[:os] == 'windows' ? 'p4.exe' : 'p4'
 p4d_exe = node[:os] == 'windows' ? 'p4d.exe' : 'p4d'
 
+directory node['perforce']['bin_dir'] do
+  recursive true
+end
+
 remote_file get_ftp_path(node['perforce']['version'], p4_exe) do
   path ::File.join(node['perforce']['bin_dir'], p4_exe)
   owner 'root'
