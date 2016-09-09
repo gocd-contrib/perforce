297eb7f5c8c28d833b17446daaf3d197f5d0de5e
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2016-09-09 18:36:48 +0530

    Make the perforce ftp server configurable, so you can use your internal server.

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,4 @@ maintainer_email 'go-cd@googlegroups.com'
 license          'MIT'
 description      'Installs/Configures perforce'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
+version          '0.2.0'
